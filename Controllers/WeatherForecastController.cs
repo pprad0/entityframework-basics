764d8e758c7b3fcd7a6969b86e99d6458fcb72b9
carburado <pedromarcosprado26@gmail.com>
2024-04-25 23:12:47 -0300

    Configuração do projeto, criação dos primeiros Controllers

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// namespace entityframework_basics.Controllers
+// {
+
+//     [ApiController]
+//     [Route("[controller]")]
+//     public class WeatherForecastController : ControllerBase
+//     {
+//         private static readonly string[] Summaries = new[]
+//         {
+//             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//         };
+//     }
+//         private readonly ILogger<WeatherForecastController> _logger;
+        
+//         public WeatherForecastController(ILogger<WeatherForecastController> logger)
+//         {
+//             _logger = logger;
+//         }
+
+//         [HttpGet(Name = "GetWeatherForecast")]
+//         public IEnumerable<WeatWeatherForecast> Get()
+// }
